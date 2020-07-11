77d72ef28bbaa1a9e18b2aec0cac3349264bd463
rbirkaur23 <64214931+rbirkaur23@users.noreply.github.com>
2020-07-11 22:10:14 +0530

    Create escape_sequence.c

---
+++ escape_sequence.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("Hello\nWorld");
+printf("Hello\tWorld");
+printf("Hello\aWorld");
+printf("Hello\bWorld");
+printf("male\\female");
+printf("Hello\"World");
+getch();
+)
